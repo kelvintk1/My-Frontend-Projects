72a797b76b403e418521581fe0b2a13ff730f19e
Kelvin Tetteh K. <tettehk537@gmail.com>
2025-07-15 19:19:46 +0000

    Progress

--- Image-URL/vite.config.js
+++ Image-URL/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      external: ['gsap'], // Add GSAP to external dependencies
+      input: './index.html'
+    }
+  }
 })
